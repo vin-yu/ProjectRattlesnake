02418be50bdcebacaa7659fadc747dfbcd7c943c
Sara Young <sara_young19@yahoo.com>
2017-06-15 14:29:27 -0700

    Added login page w/button

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -31,5 +31,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
