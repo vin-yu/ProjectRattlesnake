231c9902053f4fd5f1c757e15737acd23d1d462f
Carlos Lopez <carloslpz182@gmail.com>
2017-06-21 10:42:39 -0700

    dashboard pages not changing

--- Sample AAD/WebApp/App_Start/BundleConfig.cs
+++ Sample AAD/WebApp/App_Start/BundleConfig.cs
@@ -18,6 +18,33 @@ namespace WebApp
                         "~/Scripts/wow.min.js",
                         "~/Scripts/owl.carousel.min.js",
                         "~/Scripts/custom.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/Dashboard/jquery").Include(
+                        "~/Scripts/Dashboard/jquery.min.js",
+                        "~/Scripts/Dashboard/bootstrap.min.js",
+                        "~/Scripts/Dashboard/fastclick.js",
+                        "~/Scripts/Dashboard/nprogress.js",
+                        "~/Scripts/Dashboard/Chart.min.js",
+                        "~/Scripts/Dashboard/gauge.min.js",
+                        "~/Scripts/Dashboard/bootstrap-progressbar.min.js",
+                        "~/Scripts/Dashboard/icheck.min.js",
+                        "~/Scripts/Dashboard/skycons.js",
+                        "~/Scripts/Dashboard/jquery.flot.js",
+                        "~/Scripts/Dashboard/jquery.flot.pie.js",
+                        "~/Scripts/Dashboard/jquery.flot.time.js",
+                        "~/Scripts/Dashboard/jquery.flot.stack.js",
+                        "~/Scripts/Dashboard/jquery.flot.resize.js",
+                        "~/Scripts/Dashboard/jquery.flot.orderBars.js",
+                        "~/Scripts/Dashboard/jquery.flot.spline.min.js",
+                        "~/Scripts/Dashboard/curvedLines.js",
+                        "~/Scripts/Dashboard/date.js",
+                        "~/Scripts/Dashboard/jquery.vmap.js",
+                        "~/Scripts/Dashboard/jquery.vmap.world.js",
+                        "~/Scripts/Dashboard/jquert.vmap.sampledata.js",
+                        "~/Scripts/Dashboard/moment.min.js",
+                        "~/Scripts/Dashboard/daterangepicker.js",
+                        "~/Scripts/Dashboard/custom.min.js",
+                        "~/Scripts/Dashboard/dropzone.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -37,6 +64,16 @@ namespace WebApp
                       "~/Content/animate.css",
                       "~/Content/owl.carousel.css",
                       "~/Content/owl.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Dashboard/css").Include(
+                      "~/Content/Dashboard/bootstrap.min.css",
+                      "~/Content/Dashboard/font-awesome.min.css",
+                      "~/Content/Dashboard/nprogress.css",
+                      "~/Content/Dashboard/green.css",
+                      "~/Content/Dashboard/bootstrap-progressbar-3.3.4.min.css",
+                      "~/Content/Dashboard/jqvmap.min.css",
+                      "~/Content/Dashboard/daterangepicker.css",
+                      "~/Content/Dashboard/custom.css"));
         }
     }
 }
